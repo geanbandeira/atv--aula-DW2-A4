21d2a8f75ac6ee8fdc83e735e0d9a34a299db150
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2022-08-16 22:11:07 -0300

    Add files via upload

--- gg.js
+++ gg.js
@@ -1,4 +1,4 @@
-// Condição IF e ELSE
+/*// Condição IF e ELSE
 var greeting
 var time = new Date().getHours()
 if (time <= 12) {
@@ -36,4 +36,46 @@ switch (new Date().getDay()) {
   default:
     day = 'Dia errado'
 }
-console.log(day)
+console.log(day)*/
+
+// Condição no fim
+/*var i = 0
+do {
+  console.log(i++)
+  // console.log(++i)
+} while (i < 5)
+
+// Condição no final
+var i = 0
+while (i < 5) {
+  i += 1
+  console.log(i)
+}
+
+//for
+for (var passo = 1; passo <= 5; passo++) {
+  console.log(`${passo} `)
+}
+
+// for ... in itera sobre as propriedade/índice
+var obj = { a: 1, b: 2, c: 3 }
+for (var prop in obj) {
+  console.log('obj.' + prop + ' = ' + obj[prop])
+}*/
+
+// for ... of
+let arr = [3, 5, 7]
+
+// depois trocar para in
+for (let i of arr) {
+  console.log(i)
+}
+
+// label, break e continue
+var i = 0
+labelCancel: while (true) {
+  i += 1
+  if (i == 3) continue labelCancel
+  if (i > 5) break labelCancel
+  console.log(i)
+}
